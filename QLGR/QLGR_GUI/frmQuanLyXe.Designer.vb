b0d09b1ee107c038ad04c1d0befeae72ae033690
nththanh <16521125@gm.uit.edu.vn>
2018-05-22 17:16:54 +0700

    a

--- QLGR/QLGR_GUI/frmQuanLyXe.Designer.vb
+++ QLGR/QLGR_GUI/frmQuanLyXe.Designer.vb
@@ -28,7 +28,7 @@ Partial Class frmQuanLyXe
         Me.btnCapNhat = New System.Windows.Forms.Button()
         Me.txtMaXe = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtMaChuXe = New System.Windows.Forms.TextBox()
+        Me.txtChuXe = New System.Windows.Forms.TextBox()
         Me.txtBienSo = New System.Windows.Forms.TextBox()
         Me.txtDienThoai = New System.Windows.Forms.TextBox()
         Me.txtDiaChi = New System.Windows.Forms.TextBox()
@@ -42,22 +42,27 @@ Partial Class frmQuanLyXe
         Me.cbMaHieuXe = New System.Windows.Forms.ComboBox()
         Me.cbHieuXe = New System.Windows.Forms.ComboBox()
         Me.btDong = New System.Windows.Forms.Button()
+        Me.cbMaChuXe = New System.Windows.Forms.ComboBox()
+        Me.cbDiaChi = New System.Windows.Forms.ComboBox()
+        Me.cbDienThoai = New System.Windows.Forms.ComboBox()
+        Me.cbChuXe = New System.Windows.Forms.ComboBox()
+        Me.Label9 = New System.Windows.Forms.Label()
         CType(Me.dgvQuanLyXe, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvQuanLyXe
         '
         Me.dgvQuanLyXe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvQuanLyXe.Location = New System.Drawing.Point(85, 37)
+        Me.dgvQuanLyXe.Location = New System.Drawing.Point(202, 37)
         Me.dgvQuanLyXe.Name = "dgvQuanLyXe"
         Me.dgvQuanLyXe.ReadOnly = True
-        Me.dgvQuanLyXe.Size = New System.Drawing.Size(643, 194)
+        Me.dgvQuanLyXe.Size = New System.Drawing.Size(446, 194)
         Me.dgvQuanLyXe.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(82, 21)
+        Me.Label1.Location = New System.Drawing.Point(199, 21)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(63, 13)
         Me.Label1.TabIndex = 1
@@ -99,38 +104,41 @@ Partial Class frmQuanLyXe
         Me.Label3.TabIndex = 32
         Me.Label3.Text = "Mã Xe"
         '
-        'txtMaChuXe
+        'txtChuXe
         '
-        Me.txtMaChuXe.Location = New System.Drawing.Point(348, 332)
-        Me.txtMaChuXe.Name = "txtMaChuXe"
-        Me.txtMaChuXe.Size = New System.Drawing.Size(184, 20)
-        Me.txtMaChuXe.TabIndex = 47
+        Me.txtChuXe.Location = New System.Drawing.Point(348, 361)
+        Me.txtChuXe.Name = "txtChuXe"
+        Me.txtChuXe.ReadOnly = True
+        Me.txtChuXe.Size = New System.Drawing.Size(184, 20)
+        Me.txtChuXe.TabIndex = 47
         '
         'txtBienSo
         '
-        Me.txtBienSo.Location = New System.Drawing.Point(348, 362)
+        Me.txtBienSo.Location = New System.Drawing.Point(348, 391)
         Me.txtBienSo.Name = "txtBienSo"
         Me.txtBienSo.Size = New System.Drawing.Size(184, 20)
         Me.txtBienSo.TabIndex = 46
         '
         'txtDienThoai
         '
-        Me.txtDienThoai.Location = New System.Drawing.Point(348, 423)
+        Me.txtDienThoai.Location = New System.Drawing.Point(348, 452)
         Me.txtDienThoai.Name = "txtDienThoai"
+        Me.txtDienThoai.ReadOnly = True
         Me.txtDienThoai.Size = New System.Drawing.Size(184, 20)
         Me.txtDienThoai.TabIndex = 45
         '
         'txtDiaChi
         '
-        Me.txtDiaChi.Location = New System.Drawing.Point(348, 390)
+        Me.txtDiaChi.Location = New System.Drawing.Point(348, 419)
         Me.txtDiaChi.Name = "txtDiaChi"
+        Me.txtDiaChi.ReadOnly = True
         Me.txtDiaChi.Size = New System.Drawing.Size(184, 20)
         Me.txtDiaChi.TabIndex = 44
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(249, 365)
+        Me.Label8.Location = New System.Drawing.Point(249, 394)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(44, 13)
         Me.Label8.TabIndex = 41
@@ -139,7 +147,7 @@ Partial Class frmQuanLyXe
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(249, 426)
+        Me.Label6.Location = New System.Drawing.Point(249, 455)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(59, 13)
         Me.Label6.TabIndex = 40
@@ -148,7 +156,7 @@ Partial Class frmQuanLyXe
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(249, 393)
+        Me.Label5.Location = New System.Drawing.Point(249, 422)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(41, 13)
         Me.Label5.TabIndex = 39
@@ -157,7 +165,7 @@ Partial Class frmQuanLyXe
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(249, 335)
+        Me.Label4.Location = New System.Drawing.Point(249, 364)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(42, 13)
         Me.Label4.TabIndex = 38
@@ -200,7 +208,7 @@ Partial Class frmQuanLyXe
         'cbHieuXe
         '
         Me.cbHieuXe.FormattingEnabled = True
-        Me.cbHieuXe.Location = New System.Drawing.Point(553, 305)
+        Me.cbHieuXe.Location = New System.Drawing.Point(538, 305)
         Me.cbHieuXe.Name = "cbHieuXe"
         Me.cbHieuXe.Size = New System.Drawing.Size(175, 21)
         Me.cbHieuXe.TabIndex = 50
@@ -214,16 +222,62 @@ Partial Class frmQuanLyXe
         Me.btDong.Text = "Đóng"
         Me.btDong.UseVisualStyleBackColor = True
         '
+        'cbMaChuXe
+        '
+        Me.cbMaChuXe.FormattingEnabled = True
+        Me.cbMaChuXe.Location = New System.Drawing.Point(348, 331)
+        Me.cbMaChuXe.Name = "cbMaChuXe"
+        Me.cbMaChuXe.Size = New System.Drawing.Size(184, 21)
+        Me.cbMaChuXe.TabIndex = 52
+        '
+        'cbDiaChi
+        '
+        Me.cbDiaChi.FormattingEnabled = True
+        Me.cbDiaChi.Location = New System.Drawing.Point(538, 419)
+        Me.cbDiaChi.Name = "cbDiaChi"
+        Me.cbDiaChi.Size = New System.Drawing.Size(175, 21)
+        Me.cbDiaChi.TabIndex = 54
+        '
+        'cbDienThoai
+        '
+        Me.cbDienThoai.FormattingEnabled = True
+        Me.cbDienThoai.Location = New System.Drawing.Point(538, 452)
+        Me.cbDienThoai.Name = "cbDienThoai"
+        Me.cbDienThoai.Size = New System.Drawing.Size(175, 21)
+        Me.cbDienThoai.TabIndex = 55
+        '
+        'cbChuXe
+        '
+        Me.cbChuXe.FormattingEnabled = True
+        Me.cbChuXe.Location = New System.Drawing.Point(538, 361)
+        Me.cbChuXe.Name = "cbChuXe"
+        Me.cbChuXe.Size = New System.Drawing.Size(175, 21)
+        Me.cbChuXe.TabIndex = 58
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(249, 334)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(60, 13)
+        Me.Label9.TabIndex = 59
+        Me.Label9.Text = "Mã Chủ Xe"
+        '
         'frmQuanLyXe
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(865, 524)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.cbChuXe)
+        Me.Controls.Add(Me.cbDienThoai)
+        Me.Controls.Add(Me.cbDiaChi)
+        Me.Controls.Add(Me.cbMaChuXe)
         Me.Controls.Add(Me.btDong)
         Me.Controls.Add(Me.cbHieuXe)
         Me.Controls.Add(Me.cbMaHieuXe)
         Me.Controls.Add(Me.txtHieuXe)
-        Me.Controls.Add(Me.txtMaChuXe)
+        Me.Controls.Add(Me.txtChuXe)
         Me.Controls.Add(Me.txtBienSo)
         Me.Controls.Add(Me.txtDienThoai)
         Me.Controls.Add(Me.txtDiaChi)
@@ -253,7 +307,7 @@ Partial Class frmQuanLyXe
     Friend WithEvents btnCapNhat As Button
     Friend WithEvents txtMaXe As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents txtMaChuXe As TextBox
+    Friend WithEvents txtChuXe As TextBox
     Friend WithEvents txtBienSo As TextBox
     Friend WithEvents txtDienThoai As TextBox
     Friend WithEvents txtDiaChi As TextBox
@@ -267,4 +321,9 @@ Partial Class frmQuanLyXe
     Friend WithEvents cbMaHieuXe As ComboBox
     Friend WithEvents cbHieuXe As ComboBox
     Friend WithEvents btDong As Button
+    Friend WithEvents cbMaChuXe As ComboBox
+    Friend WithEvents cbDiaChi As ComboBox
+    Friend WithEvents cbDienThoai As ComboBox
+    Friend WithEvents cbChuXe As ComboBox
+    Friend WithEvents Label9 As Label
 End Class
--- QLGR/QLGR_GUI/frmQuanLyXe.vb
+++ QLGR/QLGR_GUI/frmQuanLyXe.vb
@@ -4,9 +4,13 @@ Imports QLGR_DTO
 
 Public Class frmQuanLyXe
     Private xeBUS As XeBUS
+    Private chuxeBUS As ChuXeBUS
 
     Private Sub frmQuanLyXe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cbHieuXe.Hide()
+        cbDiaChi.Hide()
+        cbDienThoai.Hide()
+        cbChuXe.Hide()
         xeBUS = New XeBUS()
         loadListXe()
         Dim result As Result
@@ -25,6 +29,33 @@ Public Class frmQuanLyXe
         cbMaHieuXe.DisplayMember = "mahieuxe"
         cbMaHieuXe.ValueMember = "tenhieuxe"
         cbHieuXe.DisplayMember = cbMaHieuXe.ValueMember
+
+
+        Dim listChuXe = New List(Of ChuXeDTO)
+        chuxeBUS = New ChuXeBUS()
+        result = chuxeBUS.selectAll(listChuXe)
+        If (result.FlagResult = False) Then
+            MessageBox.Show("Lấy danh sách chu xe không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            System.Console.WriteLine(result.SystemMessage)
+            Me.Close()
+            Return
+        End If
+        cbMaChuXe.DataSource = New BindingSource(listChuXe, String.Empty)
+        cbChuXe.DataSource = cbMaChuXe.DataSource
+        cbMaChuXe.DisplayMember = "machuxe"
+        cbMaChuXe.ValueMember = "tenchuxe"
+        cbChuXe.DisplayMember = cbMaChuXe.ValueMember
+
+
+
+        cbDiaChi.DataSource = cbMaChuXe.DataSource
+        cbDiaChi.DisplayMember = "diachi"
+
+        cbDienThoai.DataSource = cbMaChuXe.DataSource
+        cbDienThoai.DisplayMember = "dienthoai"
+
+
+
     End Sub
 
     Private Sub loadListXe()
@@ -57,11 +88,7 @@ Public Class frmQuanLyXe
         clMaHieuXe.DataPropertyName = "mahieuxe"
         dgvQuanLyXe.Columns.Add(clMaHieuXe)
 
-        Dim clBienSo = New DataGridViewTextBoxColumn()
-        clBienSo.Name = "bienso"
-        clBienSo.HeaderText = "Biển Số"
-        clBienSo.DataPropertyName = "bienso"
-        dgvQuanLyXe.Columns.Add(clBienSo)
+
 
         Dim clMaChuXe = New DataGridViewTextBoxColumn()
         clMaChuXe.Name = "machuxe"
@@ -69,7 +96,11 @@ Public Class frmQuanLyXe
         clMaChuXe.DataPropertyName = "machuxe"
         dgvQuanLyXe.Columns.Add(clMaChuXe)
 
-
+        Dim clBienSo = New DataGridViewTextBoxColumn()
+        clBienSo.Name = "bienso"
+        clBienSo.HeaderText = "Biển Số"
+        clBienSo.DataPropertyName = "bienso"
+        dgvQuanLyXe.Columns.Add(clBienSo)
 
         Dim myCurrencyManager As CurrencyManager = Me.BindingContext(dgvQuanLyXe.DataSource)
         myCurrencyManager.Refresh()
@@ -88,10 +119,9 @@ Public Class frmQuanLyXe
 
                 '1. Mapping data from GUI control
                 xeDTO.maxe = Convert.ToInt32(txtMaXe.Text)
-                xeDTO.bienso = txtBienSo.Text
                 xeDTO.mahieuxe = Convert.ToInt32(cbMaHieuXe.Text)
-                xeDTO.machuxe = txtMaChuXe.Text
-
+                xeDTO.machuxe = Convert.ToInt32(cbMaChuXe.Text)
+                xeDTO.bienso = txtBienSo.Text
 
 
                 '3. Insert to DB
@@ -107,9 +137,8 @@ Public Class frmQuanLyXe
                         xeDTO = CType(dgvQuanLyXe.Rows(currentRowIndex).DataBoundItem, XeDTO)
                         txtMaXe.Text = xeDTO.maxe
                         cbMaHieuXe.Text = xeDTO.mahieuxe
+                        cbMaChuXe.Text = xeDTO.machuxe
                         txtBienSo.Text = xeDTO.bienso
-                        txtMaChuXe.Text = xeDTO.machuxe
-
 
                     Catch ex As Exception
                         Console.WriteLine(ex.StackTrace)
@@ -140,18 +169,17 @@ Public Class frmQuanLyXe
                 Dim xeDTO = CType(dgvQuanLyXe.Rows(currentRowIndex).DataBoundItem, XeDTO)
                 txtMaXe.Text = xeDTO.maxe
                 cbMaHieuXe.Text = xeDTO.mahieuxe
+                cbMaChuXe.Text = xeDTO.machuxe
+                txtChuXe.Text = cbChuXe.Text
                 txtBienSo.Text = xeDTO.bienso
-                txtMaChuXe.Text = xeDTO.machuxe
-
+                txtDienThoai.Text = cbDienThoai.Text
+                txtDiaChi.Text = cbDiaChi.Text
+                txtHieuXe.Text = cbHieuXe.Text
             Catch ex As Exception
                 Console.WriteLine(ex.StackTrace)
             End Try
 
         End If
-    End Sub
-
-    Private Sub cbHieuXe_SelectedValueChanged(sender As Object, e As EventArgs) Handles cbHieuXe.SelectedValueChanged
-        txtHieuXe.Text = cbHieuXe.Text
     End Sub
 
     Private Sub btnXoa_Click(sender As Object, e As EventArgs) Handles btnXoa.Click
@@ -184,7 +212,7 @@ Public Class frmQuanLyXe
                                     txtMaXe.Text = xeDTO.maxe
                                     cbMaHieuXe.Text = xeDTO.mahieuxe
                                     txtBienSo.Text = xeDTO.bienso
-                                    txtMaChuXe.Text = xeDTO.machuxe
+                                    txtChuXe.Text = xeDTO.machuxe
 
                                 Catch ex As Exception
                                     Console.WriteLine(ex.StackTrace)
@@ -209,5 +237,18 @@ Public Class frmQuanLyXe
         Me.Close()
     End Sub
 
+    Private Sub cbChuXe_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbChuXe.SelectedIndexChanged
+        txtChuXe.Text = cbChuXe.Text
 
+
+    End Sub
+
+    Private Sub cbDienThoai_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbDienThoai.SelectedIndexChanged
+        txtDienThoai.Text = cbDienThoai.Text
+
+    End Sub
+
+    Private Sub cbDiaChi_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbDiaChi.SelectedIndexChanged
+        txtDiaChi.Text = cbDiaChi.Text
+    End Sub
 End Class
