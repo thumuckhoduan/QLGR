76a7bc41d684522fd69f949bfe1159ba52c82e19
nththanh <16521125@gm.uit.edu.vn>
2018-05-24 14:33:05 +0700

    2:32PM 5/24/2018

--- QLGR/QLGR_GUI/frmManHinhChinh.Designer.vb
+++ QLGR/QLGR_GUI/frmManHinhChinh.Designer.vb
@@ -22,96 +22,32 @@ Partial Class frmManHinhChinh
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.XeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TiếpNhậnSữaXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.QuảnLýSữaXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.QuảnLýXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.HiệuXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ThêmHiệuXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.QuảnLýHiệuXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.QuảnLýSửaXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.QuảnLýXeToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
-        Me.QuảnLýChủXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TìmKiếmToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.QuảnLíSửaXeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.XeToolStripMenuItem, Me.HiệuXeToolStripMenuItem, Me.QuảnLýSửaXeToolStripMenuItem, Me.QuảnLýXeToolStripMenuItem1, Me.QuảnLýChủXeToolStripMenuItem, Me.TìmKiếmToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.QuảnLíSửaXeToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'XeToolStripMenuItem
+        'QuảnLíSửaXeToolStripMenuItem
         '
-        Me.XeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TiếpNhậnSữaXeToolStripMenuItem, Me.QuảnLýSữaXeToolStripMenuItem, Me.QuảnLýXeToolStripMenuItem})
-        Me.XeToolStripMenuItem.Name = "XeToolStripMenuItem"
-        Me.XeToolStripMenuItem.Size = New System.Drawing.Size(32, 20)
-        Me.XeToolStripMenuItem.Text = "Xe"
+        Me.QuảnLíSửaXeToolStripMenuItem.Name = "QuảnLíSửaXeToolStripMenuItem"
+        Me.QuảnLíSửaXeToolStripMenuItem.Size = New System.Drawing.Size(100, 20)
+        Me.QuảnLíSửaXeToolStripMenuItem.Text = "Quản Lý Sửa Xe"
         '
-        'TiếpNhậnSữaXeToolStripMenuItem
+        'ContextMenuStrip1
         '
-        Me.TiếpNhậnSữaXeToolStripMenuItem.Name = "TiếpNhậnSữaXeToolStripMenuItem"
-        Me.TiếpNhậnSữaXeToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
-        Me.TiếpNhậnSữaXeToolStripMenuItem.Text = "Tiếp Nhận Sữa Xe"
-        '
-        'QuảnLýSữaXeToolStripMenuItem
-        '
-        Me.QuảnLýSữaXeToolStripMenuItem.Name = "QuảnLýSữaXeToolStripMenuItem"
-        Me.QuảnLýSữaXeToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
-        Me.QuảnLýSữaXeToolStripMenuItem.Text = "Quản Lý Sữa Xe"
-        '
-        'QuảnLýXeToolStripMenuItem
-        '
-        Me.QuảnLýXeToolStripMenuItem.Name = "QuảnLýXeToolStripMenuItem"
-        Me.QuảnLýXeToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
-        Me.QuảnLýXeToolStripMenuItem.Text = "Quản Lý Xe"
-        '
-        'HiệuXeToolStripMenuItem
-        '
-        Me.HiệuXeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ThêmHiệuXeToolStripMenuItem, Me.QuảnLýHiệuXeToolStripMenuItem})
-        Me.HiệuXeToolStripMenuItem.Name = "HiệuXeToolStripMenuItem"
-        Me.HiệuXeToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
-        Me.HiệuXeToolStripMenuItem.Text = "Hiệu Xe"
-        '
-        'ThêmHiệuXeToolStripMenuItem
-        '
-        Me.ThêmHiệuXeToolStripMenuItem.Name = "ThêmHiệuXeToolStripMenuItem"
-        Me.ThêmHiệuXeToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
-        Me.ThêmHiệuXeToolStripMenuItem.Text = "Thêm Hiệu Xe"
-        '
-        'QuảnLýHiệuXeToolStripMenuItem
-        '
-        Me.QuảnLýHiệuXeToolStripMenuItem.Name = "QuảnLýHiệuXeToolStripMenuItem"
-        Me.QuảnLýHiệuXeToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
-        Me.QuảnLýHiệuXeToolStripMenuItem.Text = "Quản Lý Hiệu Xe"
-        '
-        'QuảnLýSửaXeToolStripMenuItem
-        '
-        Me.QuảnLýSửaXeToolStripMenuItem.Name = "QuảnLýSửaXeToolStripMenuItem"
-        Me.QuảnLýSửaXeToolStripMenuItem.Size = New System.Drawing.Size(12, 20)
-        '
-        'QuảnLýXeToolStripMenuItem1
-        '
-        Me.QuảnLýXeToolStripMenuItem1.Name = "QuảnLýXeToolStripMenuItem1"
-        Me.QuảnLýXeToolStripMenuItem1.Size = New System.Drawing.Size(78, 20)
-        Me.QuảnLýXeToolStripMenuItem1.Text = "Quản Lý Xe"
-        '
-        'QuảnLýChủXeToolStripMenuItem
-        '
-        Me.QuảnLýChủXeToolStripMenuItem.Name = "QuảnLýChủXeToolStripMenuItem"
-        Me.QuảnLýChủXeToolStripMenuItem.Size = New System.Drawing.Size(103, 20)
-        Me.QuảnLýChủXeToolStripMenuItem.Text = "Quản Lý Chủ Xe"
-        '
-        'TìmKiếmToolStripMenuItem
-        '
-        Me.TìmKiếmToolStripMenuItem.Name = "TìmKiếmToolStripMenuItem"
-        Me.TìmKiếmToolStripMenuItem.Size = New System.Drawing.Size(70, 20)
-        Me.TìmKiếmToolStripMenuItem.Text = "Tìm Kiếm"
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
         '
         'frmManHinhChinh
         '
@@ -122,8 +58,7 @@ Partial Class frmManHinhChinh
         Me.IsMdiContainer = True
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "frmManHinhChinh"
-        Me.Text = "Quản Lý Gara"
-        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Text = "frmManHinhChinh"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
@@ -132,20 +67,6 @@ Partial Class frmManHinhChinh
     End Sub
 
     Friend WithEvents MenuStrip1 As MenuStrip
-    Friend WithEvents XeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents HiệuXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ThêmHiệuXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents TiếpNhậnSữaXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents QuảnLýHiệuXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents QuảnLýSữaXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents QuảnLýXeToolStripMenuItem As ToolStripMenuItem
-<<<<<<< HEAD
-    Friend WithEvents QuảnLýSửaXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents QuảnLýXeToolStripMenuItem1 As ToolStripMenuItem
-    Friend WithEvents QuảnLýChủXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents TìmKiếmToolStripMenuItem As ToolStripMenuItem
-=======
-    Friend WithEvents QuảnLýChủXeToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents TìmKiếmToolStripMenuItem As ToolStripMenuItem
->>>>>>> b1340896a1c9deeed1597928cc72aa4ccc47ecd9
+    Friend WithEvents QuảnLíSửaXeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
 End Class
