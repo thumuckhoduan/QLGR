0d86fba33f8e346fe86bbec8f8f5be4a831a6794
hoangvu98 <hoangvu131098@gmail.com>
2018-06-23 17:46:59 +0700

    update

---
+++ QLGR/QLGR_GUI/frmNhapPhuTung.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNhapPhuTung
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.cbMaPhuTung = New System.Windows.Forms.ComboBox()
+        Me.cbTenPhuTung = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(61, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mã Phụ Tùng"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(61, 88)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Tên Phụ Tùng"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(64, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Số Lượng"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(64, 220)
+        Me.Label4.Name = "Label4"
+        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Ngày Tiếp Nhận"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(183, 147)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(229, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(183, 213)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(229, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'cbMaPhuTung
+        '
+        Me.cbMaPhuTung.FormattingEnabled = True
+        Me.cbMaPhuTung.Location = New System.Drawing.Point(183, 13)
+        Me.cbMaPhuTung.Name = "cbMaPhuTung"
+        Me.cbMaPhuTung.Size = New System.Drawing.Size(229, 21)
+        Me.cbMaPhuTung.TabIndex = 8
+        '
+        'cbTenPhuTung
+        '
+        Me.cbTenPhuTung.FormattingEnabled = True
+        Me.cbTenPhuTung.Location = New System.Drawing.Point(183, 85)
+        Me.cbTenPhuTung.Name = "cbTenPhuTung"
+        Me.cbTenPhuTung.Size = New System.Drawing.Size(229, 21)
+        Me.cbTenPhuTung.TabIndex = 9
+        '
+        'frmNhapVatLieu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(645, 383)
+        Me.Controls.Add(Me.cbTenPhuTung)
+        Me.Controls.Add(Me.cbMaPhuTung)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmNhapVatLieu"
+        Me.Text = "Nhập Vật Liệu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents cbMaPhuTung As ComboBox
+    Friend WithEvents cbTenPhuTung As ComboBox
+End Class
