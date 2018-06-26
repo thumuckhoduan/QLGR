82cc7f2e985f06c94e0237b5782a60eb993d622e
hoangvu98 <38561055+hoangvu98@users.noreply.github.com>
2018-06-26 22:03:29 +0700

    Add files via upload

--- QLGR/QLGR_GUI/frmChiTietSuaChua.Designer.vb
+++ QLGR/QLGR_GUI/frmChiTietSuaChua.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmChiTietSuaChua
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,272 @@ Partial Class frmChiTietSuaChua
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txbSoLuongTon = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.txbSoLuong = New System.Windows.Forms.TextBox()
+        Me.txbDonGia = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtThanhTien = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtTienCong = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.cbTenPhuTung = New System.Windows.Forms.ComboBox()
+        Me.cbMaPhuTung = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.dgvChiTietSuaChua = New System.Windows.Forms.DataGridView()
+        Me.btCapNhat = New System.Windows.Forms.Button()
+        Me.btXoa = New System.Windows.Forms.Button()
+        Me.txbTienCong = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cbSoluongTon = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cbMaPhieuSuaChua = New System.Windows.Forms.ComboBox()
+        Me.cbDonGia = New System.Windows.Forms.ComboBox()
+        CType(Me.dgvChiTietSuaChua, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txbSoLuongTon
+        '
+        Me.txbSoLuongTon.Location = New System.Drawing.Point(186, 91)
+        Me.txbSoLuongTon.Name = "txbSoLuongTon"
+        Me.txbSoLuongTon.ReadOnly = True
+        Me.txbSoLuongTon.Size = New System.Drawing.Size(122, 20)
+        Me.txbSoLuongTon.TabIndex = 64
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(66, 98)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(43, 13)
+        Me.Label15.TabIndex = 63
+        Me.Label15.Text = "Còn Lại"
+        '
+        'txbSoLuong
+        '
+        Me.txbSoLuong.Location = New System.Drawing.Point(186, 140)
+        Me.txbSoLuong.Name = "txbSoLuong"
+        Me.txbSoLuong.Size = New System.Drawing.Size(122, 20)
+        Me.txbSoLuong.TabIndex = 61
+        '
+        'txbDonGia
+        '
+        Me.txbDonGia.Location = New System.Drawing.Point(475, 92)
+        Me.txbDonGia.Name = "txbDonGia"
+        Me.txbDonGia.ReadOnly = True
+        Me.txbDonGia.Size = New System.Drawing.Size(122, 20)
+        Me.txbDonGia.TabIndex = 59
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(354, 98)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(46, 13)
+        Me.Label12.TabIndex = 58
+        Me.Label12.Text = "Đơn Giá"
+        '
+        'txtThanhTien
+        '
+        Me.txtThanhTien.Location = New System.Drawing.Point(466, 249)
+        Me.txtThanhTien.Name = "txtThanhTien"
+        Me.txtThanhTien.ReadOnly = True
+        Me.txtThanhTien.Size = New System.Drawing.Size(122, 20)
+        Me.txtThanhTien.TabIndex = 57
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(384, 252)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(62, 13)
+        Me.Label10.TabIndex = 56
+        Me.Label10.Text = "Thành Tiền"
+        '
+        'txtTienCong
+        '
+        Me.txtTienCong.Location = New System.Drawing.Point(226, 251)
+        Me.txtTienCong.Name = "txtTienCong"
+        Me.txtTienCong.Size = New System.Drawing.Size(122, 20)
+        Me.txtTienCong.TabIndex = 55
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(66, 147)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 13)
+        Me.Label8.TabIndex = 54
+        Me.Label8.Text = "Số Lượng"
+        '
+        'cbTenPhuTung
+        '
+        Me.cbTenPhuTung.FormattingEnabled = True
+        Me.cbTenPhuTung.Location = New System.Drawing.Point(475, 48)
+        Me.cbTenPhuTung.Name = "cbTenPhuTung"
+        Me.cbTenPhuTung.Size = New System.Drawing.Size(122, 21)
+        Me.cbTenPhuTung.TabIndex = 53
+        '
+        'cbMaPhuTung
+        '
+        Me.cbMaPhuTung.FormattingEnabled = True
+        Me.cbMaPhuTung.Location = New System.Drawing.Point(186, 40)
+        Me.cbMaPhuTung.Name = "cbMaPhuTung"
+        Me.cbMaPhuTung.Size = New System.Drawing.Size(122, 21)
+        Me.cbMaPhuTung.TabIndex = 52
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(66, 43)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(72, 13)
+        Me.Label7.TabIndex = 51
+        Me.Label7.Text = "Mã Phụ Tùng"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(354, 51)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(76, 13)
+        Me.Label6.TabIndex = 50
+        Me.Label6.Text = "Tên Phụ Tùng"
+        '
+        'dgvChiTietSuaChua
+        '
+        Me.dgvChiTietSuaChua.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvChiTietSuaChua.Location = New System.Drawing.Point(55, 185)
+        Me.dgvChiTietSuaChua.Name = "dgvChiTietSuaChua"
+        Me.dgvChiTietSuaChua.Size = New System.Drawing.Size(588, 148)
+        Me.dgvChiTietSuaChua.TabIndex = 65
+        '
+        'btCapNhat
+        '
+        Me.btCapNhat.Location = New System.Drawing.Point(160, 365)
+        Me.btCapNhat.Name = "btCapNhat"
+        Me.btCapNhat.Size = New System.Drawing.Size(75, 23)
+        Me.btCapNhat.TabIndex = 66
+        Me.btCapNhat.Text = "Cập Nhật"
+        Me.btCapNhat.UseVisualStyleBackColor = True
+        '
+        'btXoa
+        '
+        Me.btXoa.Location = New System.Drawing.Point(466, 365)
+        Me.btXoa.Name = "btXoa"
+        Me.btXoa.Size = New System.Drawing.Size(75, 23)
+        Me.btXoa.TabIndex = 67
+        Me.btXoa.Text = "Xóa"
+        Me.btXoa.UseVisualStyleBackColor = True
+        '
+        'txbTienCong
+        '
+        Me.txbTienCong.Location = New System.Drawing.Point(475, 144)
+        Me.txbTienCong.Name = "txbTienCong"
+        Me.txbTienCong.Size = New System.Drawing.Size(122, 20)
+        Me.txbTienCong.TabIndex = 68
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(354, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 69
+        Me.Label1.Text = "Tiền Công"
+        '
+        'cbSoluongTon
+        '
+        Me.cbSoluongTon.FormattingEnabled = True
+        Me.cbSoluongTon.Location = New System.Drawing.Point(186, 90)
+        Me.cbSoluongTon.Name = "cbSoluongTon"
+        Me.cbSoluongTon.Size = New System.Drawing.Size(122, 21)
+        Me.cbSoluongTon.TabIndex = 70
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(226, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 13)
+        Me.Label2.TabIndex = 72
+        Me.Label2.Text = "Mã Phiếu Sữa Chữa"
+        '
+        'cbMaPhieuSuaChua
+        '
+        Me.cbMaPhieuSuaChua.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbMaPhieuSuaChua.FormattingEnabled = True
+        Me.cbMaPhieuSuaChua.Location = New System.Drawing.Point(343, 10)
+        Me.cbMaPhieuSuaChua.Name = "cbMaPhieuSuaChua"
+        Me.cbMaPhieuSuaChua.Size = New System.Drawing.Size(161, 21)
+        Me.cbMaPhieuSuaChua.TabIndex = 73
+        '
+        'cbDonGia
+        '
+        Me.cbDonGia.FormattingEnabled = True
+        Me.cbDonGia.Location = New System.Drawing.Point(475, 91)
+        Me.cbDonGia.Name = "cbDonGia"
+        Me.cbDonGia.Size = New System.Drawing.Size(122, 21)
+        Me.cbDonGia.TabIndex = 74
+        '
+        'frmChiTietSuaChua
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "frmChiTietSuaChua"
+        Me.ClientSize = New System.Drawing.Size(736, 409)
+        Me.Controls.Add(Me.cbDonGia)
+        Me.Controls.Add(Me.cbMaPhieuSuaChua)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cbSoluongTon)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txbTienCong)
+        Me.Controls.Add(Me.btXoa)
+        Me.Controls.Add(Me.btCapNhat)
+        Me.Controls.Add(Me.dgvChiTietSuaChua)
+        Me.Controls.Add(Me.txbSoLuongTon)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.txbSoLuong)
+        Me.Controls.Add(Me.txbDonGia)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtThanhTien)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.txtTienCong)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.cbTenPhuTung)
+        Me.Controls.Add(Me.cbMaPhuTung)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Name = "frmChiTietSuaChua"
+        Me.Text = "Chi Tiết Sữa Chữa"
+        CType(Me.dgvChiTietSuaChua, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txbSoLuongTon As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents txbSoLuong As TextBox
+    Friend WithEvents txbDonGia As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents txtThanhTien As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtTienCong As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents cbTenPhuTung As ComboBox
+    Friend WithEvents cbMaPhuTung As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents dgvChiTietSuaChua As DataGridView
+    Friend WithEvents btCapNhat As Button
+    Friend WithEvents btXoa As Button
+    Friend WithEvents txbTienCong As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cbSoluongTon As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cbMaPhieuSuaChua As ComboBox
+    Friend WithEvents cbDonGia As ComboBox
 End Class
--- QLGR/QLGR_GUI/frmChiTietSuaChua.vb
+++ QLGR/QLGR_GUI/frmChiTietSuaChua.vb
@@ -1,3 +1,256 @@
-﻿Public Class frmChiTietSuaChua
+﻿Imports QLGR_BUS
+Imports Utility
+Imports QLGR_DTO
 
+Public Class frmChiTietSuaChua
+    Private chitietsuachuaBUS As ChiTietSuaChuaBUS
+    Private phieusuachuaBUS As PhieuSuaChuaBUS
+    Private phutungBUS As PhuTungBUS
+    Private listphutung As List(Of PhuTungDTO)
+    Private listphieu As List(Of PhieuSuaChuaDTO)
+    Private list As List(Of dgvChiTietSuaChua)
+    Private Sub frmChiTietSuaChua_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        chitietsuachuaBUS = New ChiTietSuaChuaBUS()
+        phutungBUS = New PhuTungBUS()
+        listphutung = New List(Of PhuTungDTO)
+        listphieu = New List(Of PhieuSuaChuaDTO)
+        cbSoluongTon.Hide()
+        cbDonGia.Hide()
+        list = New List(Of dgvChiTietSuaChua)
+        phieusuachuaBUS = New PhieuSuaChuaBUS()
+        phieusuachuaBUS.selectAll(listphieu)
+        cbMaPhieuSuaChua.DataSource = New BindingSource(listphieu, String.Empty)
+        cbMaPhieuSuaChua.DisplayMember = "maphieusuachua"
+        cbMaPhieuSuaChua.ValueMember = "maphieusuachua"
+
+        loaddgv()
+
+        loadcb()
+    End Sub
+
+    Private Sub loadcb()
+        phutungBUS.selectAll(listphutung)
+        cbMaPhuTung.DataSource = New BindingSource(listphutung, String.Empty)
+        cbMaPhuTung.DisplayMember = "maphutung"
+        cbMaPhuTung.ValueMember = "maphutung"
+
+        cbTenPhuTung.DataSource = cbMaPhuTung.DataSource
+        cbMaPhuTung.ValueMember = "tenphutung"
+        cbTenPhuTung.DisplayMember = cbMaPhuTung.ValueMember
+
+        cbSoluongTon.DataSource = cbMaPhuTung.DataSource
+        cbMaPhuTung.ValueMember = "soluongton"
+        cbSoluongTon.DisplayMember = cbMaPhuTung.ValueMember
+
+        cbDonGia.DataSource = cbMaPhuTung.DataSource
+        cbMaPhuTung.ValueMember = "dongia"
+        cbDonGia.DisplayMember = cbMaPhuTung.ValueMember
+
+
+        cbMaPhuTung.ValueMember = "maphutung"
+    End Sub
+
+    Private Sub cbSoluongTon_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbSoluongTon.SelectedIndexChanged
+        txbSoLuongTon.Text = cbSoluongTon.Text
+    End Sub
+
+
+
+    Private Sub dgvChiTietSuaChua_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvChiTietSuaChua.CellContentClick
+        ' Get the current cell location.
+        Dim currentRowIndex As Integer = dgvChiTietSuaChua.CurrentCellAddress.Y 'current row selected
+        'Dim x As Integer = dgvListHS.CurrentCellAddress.X 'curent column selected
+
+        ' Write coordinates to console for debugging
+        'Console.WriteLine(y.ToString + " " + x.ToString)
+
+        'Verify that indexing OK
+        If (-1 < currentRowIndex And currentRowIndex < dgvChiTietSuaChua.RowCount) Then
+            Try
+                Dim phutung = CType(dgvChiTietSuaChua.Rows(currentRowIndex).DataBoundItem, dgvChiTietSuaChua)
+                cbMaPhuTung.Text = phutung.maphutung
+                cbTenPhuTung.Text = phutung.tenphutung
+                txbSoLuong.Text = phutung.soluong
+                txbDonGia.Text = phutung.dongia
+                txbTienCong.Text = phutung.tiencong
+            Catch ex As Exception
+                Console.WriteLine(ex.StackTrace)
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub loaddgv()
+        Dim maphieu = Convert.ToInt32(cbMaPhieuSuaChua.SelectedValue)
+        chitietsuachuaBUS.selectAll_bymaphieu(maphieu, list)
+
+
+        dgvChiTietSuaChua.Columns.Clear()
+        dgvChiTietSuaChua.DataSource = Nothing
+
+        dgvChiTietSuaChua.AutoGenerateColumns = False
+        dgvChiTietSuaChua.AllowUserToAddRows = False
+        dgvChiTietSuaChua.DataSource = list
+
+        Dim clmaphutung = New DataGridViewTextBoxColumn()
+        clmaphutung.Name = "maphutung"
+        clmaphutung.HeaderText = "Mã Phụ Tùng"
+        clmaphutung.DataPropertyName = "maphutung"
+        dgvChiTietSuaChua.Columns.Add(clmaphutung)
+
+        Dim cltenphutung = New DataGridViewTextBoxColumn()
+        cltenphutung.Name = "tenphutung"
+        cltenphutung.HeaderText = " Tên Phụ Tùng"
+        cltenphutung.DataPropertyName = "tenphutung"
+        dgvChiTietSuaChua.Columns.Add(cltenphutung)
+
+        Dim clsoluong = New DataGridViewTextBoxColumn()
+        clsoluong.Name = "soluong"
+        clsoluong.HeaderText = "Số Lượng"
+        clsoluong.DataPropertyName = "soluong"
+        dgvChiTietSuaChua.Columns.Add(clsoluong)
+
+        Dim cldongia = New DataGridViewTextBoxColumn()
+        cldongia.Name = "dongia"
+        cldongia.HeaderText = "Đơn Giá"
+        cldongia.DataPropertyName = "dongia"
+        dgvChiTietSuaChua.Columns.Add(cldongia)
+
+        Dim cltiencong = New DataGridViewTextBoxColumn()
+        cltiencong.Name = "tiencong"
+        cltiencong.HeaderText = "Tiền Công"
+        cltiencong.DataPropertyName = "tiencong"
+        dgvChiTietSuaChua.Columns.Add(cltiencong)
+    End Sub
+
+    Private Sub cbMaPhieuSuaChua_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbMaPhieuSuaChua.SelectedIndexChanged
+        Try
+            loaddgv()
+            loaddata()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub loaddata()
+        Dim test = True
+        Try
+            Dim x = Convert.ToInt32(cbMaPhuTung.SelectedValue)
+            For Each item In list
+                If (item.maphutung = x) Then
+                    txbDonGia.Text = item.dongia
+                    txbSoLuong.Text = item.soluong
+                    txbTienCong.Text = item.tiencong
+                    test = False
+                End If
+            Next
+        Catch ex As Exception
+        End Try
+        If (test) Then
+            txbTienCong.Text = "0"
+            txbSoLuong.Text = "0"
+        End If
+    End Sub
+    Private Sub cbMaPhuTung_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbMaPhuTung.SelectedIndexChanged
+        loaddata()
+    End Sub
+
+    Private Sub btCapNhat_Click(sender As Object, e As EventArgs) Handles btCapNhat.Click
+        Dim test = True
+        Dim chitiet = New ChiTietSuaChuaDTO
+        Dim phutung = New PhuTungDTO()
+        For Each item In list
+            If (item.maphutung = cbMaPhuTung.Text) Then
+                chitiet.machitietsuachua = item.machitietsuachua
+                chitiet.maphieusuachua = item.maphieusuachua
+                chitiet.maphutung = item.maphutung
+                chitiet.soluong = txbSoLuong.Text
+                chitiet.dongia = txbDonGia.Text
+                chitiet.tiencong = txbTienCong.Text
+                chitietsuachuaBUS.update(chitiet)
+
+                phutung.maphutung = cbMaPhuTung.Text
+                phutung.tenphutung = cbTenPhuTung.Text
+                phutung.soluongton = cbSoluongTon.Text - txbSoLuong.Text + item.soluong
+                phutung.dongia = cbDonGia.Text
+                phutungBUS.update(phutung)
+                test = False
+            End If
+        Next
+        If (test) Then
+
+            Dim ma = 0
+            chitietsuachuaBUS.buildMaChiTietSuaChua(ma)
+            chitiet.machitietsuachua = ma
+            chitiet.maphieusuachua = cbMaPhieuSuaChua.Text
+            chitiet.maphutung = cbMaPhuTung.Text
+            chitiet.soluong = txbSoLuong.Text
+            chitiet.dongia = txbDonGia.Text
+            chitiet.tiencong = txbTienCong.Text
+            chitietsuachuaBUS.insert(chitiet)
+
+            phutung.maphutung = cbMaPhuTung.Text
+            phutung.tenphutung = cbTenPhuTung.Text
+            phutung.soluongton = cbSoluongTon.Text - txbSoLuong.Text
+            phutung.dongia = cbDonGia.Text
+            phutungBUS.update(phutung)
+        End If
+        loaddgv()
+        loadcb()
+    End Sub
+
+    Private Sub btXoa_Click(sender As Object, e As EventArgs) Handles btXoa.Click
+        ' Get the current cell location.
+        Dim currentRowIndex As Integer = dgvChiTietSuaChua.CurrentCellAddress.Y 'current row selected
+
+        Dim phutung = CType(dgvChiTietSuaChua.Rows(currentRowIndex).DataBoundItem, dgvChiTietSuaChua)
+        'Verify that indexing OK
+        If (-1 < currentRowIndex And currentRowIndex < dgvChiTietSuaChua.RowCount) Then
+            Select Case MsgBox("Bạn có thực sự muốn xóa phụ tùng có mã số: " + Convert.ToString(phutung.maphutung), MsgBoxStyle.YesNo, "Information")
+                Case MsgBoxResult.Yes
+                    Try
+                        Dim result As Result
+                        Dim maphieu As Integer
+                        Dim phutungDTO = New PhuTungDTO()
+
+                        maphieu = phutung.machitietsuachua
+                        phutungDTO.maphutung = phutung.maphutung
+                        phutungDTO.tenphutung = phutung.tenphutung
+                        phutungDTO.soluongton = cbSoluongTon.Text + phutung.soluong
+                        phutungDTO.dongia = phutung.dongia
+                        phutungBUS.update(phutungDTO)
+
+                        result = chitietsuachuaBUS.delete(maphieu)
+
+
+
+                        If (result.FlagResult = True) Then
+                            loadcb()
+                            loaddgv()
+                            ' Hightlight the next row on table
+                            If (currentRowIndex >= dgvChiTietSuaChua.Rows.Count) Then
+                                currentRowIndex = currentRowIndex - 1
+                            End If
+                            If (currentRowIndex >= 0) Then
+                                dgvChiTietSuaChua.Rows(currentRowIndex).Selected = True
+                            End If
+
+                            MessageBox.Show("Xóa chi tiết sữa chữa thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Else
+                            MessageBox.Show("Xóa chi tiết sữa chữa không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            System.Console.WriteLine(result.SystemMessage)
+                        End If
+                    Catch ex As Exception
+                        Console.WriteLine(ex.StackTrace)
+                    End Try
+                Case MsgBoxResult.No
+                    Return
+            End Select
+
+
+        End If
+    End Sub
+
+    Private Sub cbDonGia_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbDonGia.SelectedIndexChanged
+        txbDonGia.Text = cbDonGia.Text
+    End Sub
 End Class
